d12076845ddd7198c0ac7eb1d5af27c0fdc35e4a
hannakpa <petrakovahanna@gmail.com>
2025-10-07 13:54:36 +0200

    Wiremok_simpleTest_JSONResponseWorking

--- src/test/java/org/ginji/hometoolsbackend/cucumber/steps/GetProduct_Wiremock.java
+++ src/test/java/org/ginji/hometoolsbackend/cucumber/steps/GetProduct_Wiremock.java
@@ -8,31 +8,40 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
 import org.wiremock.spring.EnableWireMock;
+import org.springframework.beans.factory.annotation.Value;
 
 
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @EnableWireMock
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GetProduct_Wiremock {
+    @Value("${wiremock.server.baseUrl}")
+    private String wireMockUrl;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Given("the product {string} exists")
     public void firstStep(String productName) {
         System.out.print("LOG " + productName);
-        stubFor(get("/ping").willReturn(ok("pong")));
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/ping", String.class);
+        //stubFor(get("/ping").willReturn(ok("pong")));
+        String mockResponse = "{\"message\": \"Hello, Baeldung!\"}";
+        //return a Json
+        stubFor(get("/api/products")
+                .willReturn(okJson(mockResponse)));
+        ResponseEntity<String> response = restTemplate.getForEntity(wireMockUrl+"/api/greeting", String.class);
 
         if (response.getStatusCode().is2xxSuccessful()) {
             System.out.println("Response: " + response.getBody());
         }
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(mockResponse, response.getBody());
     }
 
     @When("I look for the product {string}")
